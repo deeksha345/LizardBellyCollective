096728d2d9b3808a01f1fc3437f37c97b9627f79
deeksha345 <34255011+deeksha345@users.noreply.github.com>
2020-05-19 14:15:50 -0700

    Deeksha (#6)
    * got the backdrop working how I wanted it!!!!
    
    * made it so the sidebar is formatted properly bottom section comes after and is affected by top section
    
    * cosmetic changes
    
    * added routing from home page to about page. active links works
    
    * created containers for all the different pages and routing to each works
    
    * added admin page
    
    * added media queries for phone settings. created a phone sidebar and sidebar toggle
    
    * fixed an error with the sidebar nav items
    
    * styling home page properly for phone mode
    
    * adding routing to artists page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Layout from './hoc/Layout/Layout';
 import Home from './containers/Home/Home';
 import About from './containers/About/About';
 import Events from './containers/Events/Events';
+import Artists from './containers/Artists/Artists';
 import Media from './containers/Media/Media';
 import Merch from './containers/Merch/Merch';
 import Epk from './containers/EPK/Epk';
@@ -22,7 +23,7 @@ class App extends Component {
                   <Route path="/" exact component={Home} />
                   <Route path="/about" exact component={About} />
                   <Route path="/events" exact component={Events} />
-                  <Route path="/artists" exact component={Home} />
+                  <Route path="/artists" exact component={Artists} />
                   <Route path="/media" exact component={Media} />
                   <Route path="/merch" exact component={Merch} />
                   <Route path="/epk" exact component={Epk} />
---
+++ src/containers/Artists/Artists.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import classes from './Artists.css';
+
+class Artists extends Component {
+    render() {
+        return (
+            <div className={classes.Artists}>
+                <h1>Artists!</h1>
+            </div>
+        );
+    }
+}
+
+export default Artists;
