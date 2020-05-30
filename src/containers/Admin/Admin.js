a43c9ddcee213db697f82493726ed1db64f833f4
deeksha345 <34255011+deeksha345@users.noreply.github.com>
2020-05-30 12:56:59 -0700

    Gus firebase (#10)
    * Added the cards and mostly set up axios/ firebase
    
    * now pulls from firebase
    
    * artist page sends request on 1st render
    
    * added ArtistDataRow for admin page. beginning work with flexbox
    
    * about to try a new method for communicating with firebase
    
    * more work with database, just bookmarking
    
    * database up/down finally working, select artist and edit their info
    
    * works with images as well
    
    * admin page works for artists
    
    * working editor. Next step cleanup/ style, then events page
    
    Co-authored-by: Gus <gusmcmanus88@gmail.com>

--- src/containers/Admin/Admin.js
+++ src/containers/Admin/Admin.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import classes from './Admin.css';
+import DataRow from '../../components/ArtistDataRow/ArtistDataRow';
 
 class Admin extends Component {
     render() {
         return (
             <div className={classes.Admin}>
                 <h1>Admin login!</h1>
+                <DataRow name={'test'} bio={'test bio'}/>
             </div>
         );
     }
--- src/containers/Artists/Artists.js
+++ src/containers/Artists/Artists.js
@@ -1,14 +1,46 @@
-import React, { Component } from 'react';
+import React from 'react';
 import classes from './Artists.css';
+import IndexCards from '../../components/IndexCards/IndexCards';
+import Firebase from '../../Firebase';
 
-class Artists extends Component {
-    render() {
-        return (
-            <div className={classes.Artists}>
-                <h1>Artists!</h1>
-            </div>
-        );
+const Artists = () => {
+
+  const [artistInfo, setArtistInfo] = React.useState();
+
+  React.useEffect( () => {
+    const fetchData = async () => {
+      const db = Firebase.firestore();
+      const data = await db.collection('artists').get();
+      setArtistInfo(data)
     }
-}
+    fetchData()
+  }, [] )
+
+  const clickArtistHandler = (key) => {
+    console.log(key)
+  }
+
+  return(
+        
+        <div className={classes.Artists}>
+          <h1>Artists!</h1>
+          <div className={classes.Showcase}>
+          {artistInfo ? artistInfo.docs.map (artist => (
+                <div className={classes.Item} key={artist.id}>
+                <IndexCards
+                    key={artist.id}
+                    fbKey={artist.id}
+                    title={artist.data().name}
+                    description={artist.data().bio}
+                    imgSource={artist.data().image}
+                    click={clickArtistHandler}
+                />
+                </div>
+            )) : <p>LOADING...</p>}
+          </div>
+        </div>
+          
+    ); 
+};
 
 export default Artists;
